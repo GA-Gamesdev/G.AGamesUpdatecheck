1a8593516bd64200be69e246465b5535df22e305
GA-Gamesdev <helpme704586@gmail.com>
2026-02-25 17:44:37 -0500

    Update GAv1 kaplay.js

--- G.AGames Start/GAv1 kaplay.js
+++ G.AGames Start/GAv1 kaplay.js
@@ -1,12 +1,17 @@
 //gagames start for kaplay
-kaplay()
+kaplay(
+width:600,
+height:600,
+)
 scene("loadgame",()=>{
 setBackground(0,0,0)
 loadSprite("kaplay","https://image2url.com/r2/default/images/1772056803247-6cfb2277-0791-4e76-8ce9-d5bee05420dd.png")
 loadSprite("ga","https://image2url.com/r2/default/images/1772058295528-ccb9ac04-b3b6-4e72-b32f-01b373fa06fc.png")
 
 add([
-sprite("ga")
+sprite("ga"),
+anchor("center"),
+pos(150,150)
 ])
 })
 go("loadgame")
