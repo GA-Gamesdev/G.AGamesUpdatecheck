3da07af4820869b15a27ff806f45db08130cf99c
GA-Gamesdev <helpme704586@gmail.com>
2026-02-25 17:02:03 -0500

    Create GAv1 kaplay.js

---
+++ G.AGames Start/GAv1 kaplay.js
@@ -0,0 +1,9 @@
+//gagames start for kaplay
+kaplay()
+setBackground(0,0,0)
+loadSprite("kaplay","https://image2url.com/r2/default/images/1772056803247-6cfb2277-0791-4e76-8ce9-d5bee05420dd.png")
+loadSprite("ga","https://image2url.com/r2/default/images/1772056889685-e99e197f-9708-40fd-8176-fdb2283452c3.png")
+
+add([
+sprite("ga")
+])
